a14521ca3cc67e2d24dbdb04243a9fd6eccf041b
NHAT LINH <nhatlinh.6449@gmail.com>
2023-11-12 21:16:37 +0700

    121123 2

--- Algorithms/SieveOfEratos.cpp
+++ Algorithms/SieveOfEratos.cpp
@@ -7,8 +7,8 @@ using namespace std;
 const long long MOD = 1e9 + 9;
 const long long INF = 1e12 + 100;
 
-int main(){
-    int n; n = 1000;
+vector <int> find_Prime(int n)
+{
     vector <bool> isPrime(n+1, true);
     vector <int> res;
     isPrime[0] = isPrime[1] = false;
@@ -22,5 +22,28 @@ int main(){
             }
         }
     }
-    for (auto i : res) cout << i << " ";    
+    return res; 
+}
+void Integer_Factorization()
+{
+    int n; n = 19456;
+    vector <int> primes = find_Prime(sqrt(n) + 1);
+    vector <int> ans;
+    for (auto i : primes){
+        while(n % i == 0)
+        {
+            ans.push_back(i);
+            n /= i;
+        }
+    }
+    for (auto i : ans)
+        cout << i << " ";
+}
+
+
+
+int main(){
+   cin.tie(0); cin.sync_with_stdio(false);
+   Integer_Factorization();
+
 }
